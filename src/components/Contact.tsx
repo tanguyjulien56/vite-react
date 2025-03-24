58d68fef2e0b7d2e3b38bc4b932c07e3a94df205
Julien <tanguyjulien@hotmail.fr>
2025-03-24 21:19:00 +0100

    all done

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,39 +2,33 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import GitHubIcon from "./ui/icon/GitHubIcon";
-import LinkedInIcon from "./ui/icon/LinkedInIcon";
-import NotionIcon from "./ui/icon/NotionIcon";
-import InstagramIcon from "./ui/icon/InstagramIcon";
 import FacebookIcon from "./ui/icon/FacebookIcon";
+import InstagramIcon from "./ui/icon/InstagramIcon";
 
 export default function Contact() {
   return (
     <div className="sticky w-full bottom-10 right-0 p-5 z-50 lg:hidden">
-      <ul className=" hidden lg:flex items-start gap-2">
-          <Link href="https://www.instagram.com/lebretonalois/">
-            <Button
-              variant="outline"
-              className=" hover:shadow-md hover:shadow-purple-500/10"
-            >
-              <InstagramIcon
-                size={16}
-                className="text-foreground color-primary"
-              />
-            </Button>
-          </Link>
-          <Link href="https://www.facebook.com/alois.lebreton.9">
-            <Button
-              variant="outline"
-              className=" hover:shadow-md hover:shadow-purple-500/10"
-            >
-              <FacebookIcon
-                size={16}
-                className="text-foreground color-primary"
-              />
-            </Button>
-          </Link>
-        </ul>
+      <ul className="flex gap-4 justify-end">
+        <Link href="https://www.instagram.com/lebretonalois/">
+          <Button
+            variant="outline"
+            className=" hover:shadow-md hover:shadow-purple-500/10"
+          >
+            <InstagramIcon
+              size={16}
+              className="text-foreground color-primary"
+            />
+          </Button>
+        </Link>
+        <Link href="https://www.facebook.com/alois.lebreton.9">
+          <Button
+            variant="outline"
+            className=" hover:shadow-md hover:shadow-purple-500/10"
+          >
+            <FacebookIcon size={16} className="text-foreground color-primary" />
+          </Button>
+        </Link>
+      </ul>
     </div>
   );
 }
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,61 +1,85 @@
 "use client";
 import { motion } from "framer-motion";
-import Section from "./Section";
+import { Star } from "lucide-react";
 import { Card } from "./ui/card";
+import { Highlight } from "./ui/hero-highlight";
+
+interface Competency {
+  name: string;
+  rating: number;
+}
 
 const cardVariants = {
   hidden: { opacity: 0, y: 20 },
   visible: { opacity: 1, y: 0 },
 };
 
-const titleVariants = {
-  hidden: { opacity: 0, y: -20 },
-  visible: { opacity: 1, y: 0 },
-};
+const skills: Competency[] = [
+  { name: "Gestion d’une offre commerciale", rating: 3 },
+  { name: "Gestion réseaux sociaux", rating: 4 },
+  { name: "Conseil client", rating: 3 },
+  { name: "Préparation d’un vélo", rating: 4 },
+];
 
-export default function Entreprise() {
+export default function Skills() {
   return (
-    <Section className="flex flex-col gap-5 items-start">
-      <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 text-primary">
+    <div className="flex flex-col gap-5 items-start p-4">
+      <h2 className="text-3xl font-semibold tracking-tight text-primary">
         Mes skills !
       </h2>
-      <div className="grid md:grid-cols-2 gap-5 h-max-96">
-        <motion.div
-          className="flex-[1]"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          variants={cardVariants}
-          transition={{ duration: 0.5 }}
-        >
-          <Card className="p-4 hover:shadow-lg border-1px hover:shadow-red-500/10">
-            <h3 className="scroll-m-20 text-lg font-semibold tracking-tight">
-              Mon auto evaluation
-            </h3>
-            <p className="leading-7 [&:not(:first-child)]:mt-6">
-              blalbalblalblalblabla
-            </p>
-          </Card>
-        </motion.div>
-
-        <motion.div
-          className="flex-[1]"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          variants={cardVariants}
-          transition={{ duration: 0.5 }}
-        >
-          <Card className=" p-4 hover:shadow-lg h-full items-start justify-start border-1px hover:shadow-red-500/10">
-            <h3 className="scroll-m-20 text-lg font-semibold tracking-tight">
-              Les compétences restantes à aquerir{" "}
-            </h3>
-            <p className="leading-7 [&:not(:first-child)]:mt-6">
-              blalbalblalblalblabla
-            </p>
-          </Card>
-        </motion.div>
-      </div>
-    </Section>
+      <motion.div
+        className="grid md:grid-cols-2 gap-5 h-max-96"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        variants={cardVariants}
+        transition={{ duration: 0.5 }}
+      >
+        <Card className="p-4 hover:shadow-lg border-1px hover:shadow-red-500/10">
+          <h3 className="text-lg font-semibold tracking-tight mb-4">
+            Mon auto-évaluation
+          </h3>
+          {skills.map((comp, i) => (
+            <div key={i} className="flex justify-between items-center mb-2">
+              <span>{comp.name}</span>
+              <div className="flex">
+                {[...Array(5)].map((_, j) => (
+                  <Star
+                    key={j}
+                    size={16}
+                    className={
+                      j < comp.rating ? "text-currentColor" : "text-gray-300"
+                    }
+                    fill={j < comp.rating ? "currentColor" : "none"}
+                  />
+                ))}
+              </div>
+            </div>
+          ))}
+        </Card>{" "}
+        <Card className="p-4 hover:shadow-lg border-1px hover:shadow-red-500/10">
+          <h3 className="text-lg font-semibold tracking-tight mb-4">
+            Compétences qu'il me reste à acquérir
+          </h3>
+          <ul className="list-disc pl-6 space-y-2">
+            <li>
+              <Highlight>Connaissance des produits</Highlight> Maîtriser les
+              gammes proposées pour mieux orienter et rassurer le client sur le
+              choix du vélo ou produit idéal.
+            </li>
+            <li>
+              <Highlight>Maîtrise des logiciels</Highlight> Améliorer mon
+              efficacité en utilisant les logiciels de l'entreprise pour offrir
+              un service plus rapide.
+            </li>
+            <li>
+              <Highlight>Études posturales</Highlight> Développer mes
+              compétences pour ajuster la posture des clients et optimiser leur
+              performance (secondaire).
+            </li>
+          </ul>
+        </Card>
+      </motion.div>
+    </div>
   );
 }
